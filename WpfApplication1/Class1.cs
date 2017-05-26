d713c8f67aaa831c50eee74a4e24e508b6eab0b7
Vincenzo <vcn.genna@gmail.com>
2017-05-26 10:36:53 +0200

    Add project files.

---
+++ WpfApplication1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
+{
+
+    class Class1
+    {
+        private String str;
+
+        public Class1(string newstr)
+        {
+            str = newstr;
+        }
+
+        public void printStr() {
+            MessageBox.Show(str);
+        }
+
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioButton1.IsChecked == true)
+            {
+                MessageBox.Show("Ciao");
+            }
+            else
+            {
+                if (radioButton2.IsChecked == true)
+                {
+                    Class1 obj = new Class1("Cancaro");
+                    obj.printStr();
+                }
+                else
+                {
+                    MessageBox.Show("Obbligatorio selezionare una scelta!");
+                }
+            }
+        }
+    }
+}
